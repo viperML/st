e25714beee4ed01e6542d985f344683c5409234f
Fernando Ayats <ayatsfer@gmail.com>
2021-06-27 18:50:31 +0200

    No lib32 requirement

--- ABS_cleaninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-diff -b -u config.def.h config.theme.h > patches/00-theme.diff
-cat patches/00-theme.diff
-sudo pacman -Rns st-git --noconfirm
-git clean -dfX
-rm -rf src
-makepkg -sric --noconfirm
---
+++ clean_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if cat /etc/os-release | grep -q "ID=arch"; then
+    diff -b -u config.def.h config.theme.h > patches/00-theme.diff
+    cat patches/00-theme.diff
+    sudo pacman -Rns st-git --noconfirm
+    git clean -dfX
+    rm -rf src
+    makepkg -sric --noconfirm
+else
+    echo "This script only works in Arch Linux"
+fi
